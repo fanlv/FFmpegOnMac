78c1f54e414403d4548ab75c0bff9c93fbc39508
fanlv <fanlv@bytedance.com>
2018-12-28 11:09:35 +0800

    feat:add demo
    Change-Id: I769267f10032f2a9d51916493710f0846fa6257d

---
+++ FFmpegOnMac/cmake-build-debug/CMakeFiles/FFmpegOnMac.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FFmpegOnMac.dir/main.cpp.o"
+  "FFmpegOnMac.pdb"
+  "FFmpegOnMac"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FFmpegOnMac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
